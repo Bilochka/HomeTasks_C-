0cf365e458e63d38ea3418ccd23851cad846910a
Alla Bevz <bevzalla@gmail.com>
2018-01-26 15:04:07 +0200

    my_commit

---
+++ FirstEF6App_Code_First/SoccerContext.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+
+namespace FirstEF6App_Code_First
+{
+    public class SoccerContext : DbContext
+    {
+        public SoccerContext()
+            : base("dbconnection")
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
---
+++ FirstEF6App_Code_First/Team.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstEF6App_Code_First
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(180, ErrorMessage = "To long"), MinLength(3), Required]
+        public string Name { get; set; } // название команды
+        [MaxLength(180, ErrorMessage = "To long"), MinLength(3), Required]
+        public string Coach { get; set; } // тренер
+
+
+
+        /*
+        public virtual ICollection<Player> Players { get; set; }
+        
+        public Team()
+        {
+            Players = new List<Player>();
+        }*/
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
